a2a3125a3d8892acce1a11a2658081e74a48d3fb
kimgupta <113166370+kimgupta@users.noreply.github.com>
2023-07-31 18:17:05 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Min Subsets with Consecutive Numbers/min-subsets-with-consecutive-numbers.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.numofsubset(a, n));
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+
+
+//User function Template for Java
+
+
+class Solution {
+    
+    public static int numofsubset(int arr[], int n)
+    {
+         // Your code goes here
+         Arrays.sort(arr);
+         int count=0;
+         for(int i=0;i<arr.length-1;i++){
+             if(arr[i+1]-arr[i]>1)
+             count++;
+         }
+         return count+1;
+    }
+}
